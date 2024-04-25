bdadae0b8d79a90901fb7180ccb1d9dfce1eb945
Adam Domoslawski <37457625+RabDou44@users.noreply.github.com>
2024-04-25 02:15:32 +0200

    end

--- src/run.sh
+++ src/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# python job_test3.py --stopwords=.\stopwords.txt  .\reviews_devset.json > .\output.txt 
-python job_test3.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop --stopwords=stopwords.txt hdfs:///user/dic24_shared/amazon-reviews/full/reviewscombined.json > output.txt
+# python job_test3.py --stopwords=stopwords.txt  reviews_devset.json > output.txt 
+python job_test3.py --hadoop-streaming-jar /usr/lib/hadoop/tools/lib/hadoop-streaming-3.3.5.jar -r hadoop --stopwords=stopwords.txt hdfs:///user/dic24_shared/amazon-reviews/full/reviewscombined.json > ../output.txt
